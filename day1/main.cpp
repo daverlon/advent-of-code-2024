a65141dcafdf0003c85fdea824b8b5719d312e08
David <74102444+daverlon@users.noreply.github.com>
2025-10-03 21:58:19 +0800

    d1p1

---
+++ day1/main.cpp
@@ -0,0 +1,64 @@
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+void get_numbers(const string & line, array<int, 2> & out) {
+  array<string, 2> s;
+  int n = 0;
+  for (auto & c : line) {
+    if (c != ' ') {
+      s[n].push_back(c);
+    }
+    else {
+      if (n == 0) n = 1;
+    }
+  } 
+  out[0] = stoi(s[0]);
+  out[1] = stoi(s[1]);
+}
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " <file>" << endl; 
+    exit(EXIT_FAILURE);
+  }
+
+  cout << "Using input file: " << argv[1] << endl;
+
+  fstream f(argv[1]);
+  cout << argv[1] << endl;
+
+  vector<int> arr1;
+  vector<int> arr2;
+  string line;
+  while (getline(f, line)) {
+    array<int, 2> Ns;
+    get_numbers(line, Ns);
+    arr1.push_back(Ns[0]);
+    arr2.push_back(Ns[1]);
+  }
+  sort(arr1.begin(), arr1.end(), greater<int>());
+  sort(arr2.begin(), arr2.end(), greater<int>());
+
+  int sum = 0;
+
+  int l = arr1.size();
+  int i;
+  for (i = 0; i < l; i++) {
+    int d = abs(arr1[i] - arr2[i]);
+    sum += d;
+  }
+  
+  cout << "Sum: " << sum << endl;
+
+  f.close();
+
+  exit(EXIT_SUCCESS);
+}
---
+++ day1/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ main.cpp -std=c++20 -o main && ./main "testinput.txt"
